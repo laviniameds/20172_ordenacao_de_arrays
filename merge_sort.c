6576a11f34d049ffbbcdcd44b86459baaf4d39d6
laviniameds <laviniameds@icloud.com>
2017-12-23 15:39:04 -0300

    ->merge sort implementado; ->results gerado;

--- merge_sort.c
+++ merge_sort.c
@@ -1,6 +1,36 @@
 #include "sort.h"
 
+void merge(int a[],int i1,int j1,int i2,int j2){
+ int *temp = (int*)malloc(sizeof(int)*((j1-i1)+(j2-i2)+2));
+ int i,j,k;
+ i=i1;
+ j=i2;
+ k=0;
+ while(i<=j1 && j<=j2){
+   if(a[i]< a[j])
+    temp[k++]=a[i++];
+   else
+    temp[k++]=a[j++];
+ }
+ while(i<=j1)
+    temp[k++]=a[i++];
+ while(j<=j2)
+    temp[k++]=a[j++];
+ for(i=i1,j=0;i<=j2;i++,j++)
+    a[i]=temp[j];
+ free(temp);
+}
+
+void merge_sort_top(int a[],int primeiro,int ultimo){
+  int meio;
+  if(primeiro<ultimo) {
+    meio=(primeiro+ultimo)/2;
+    merge_sort_top(a,primeiro,meio);
+    merge_sort_top(a,meio+1,ultimo);
+    merge(a,primeiro,meio,meio+1,ultimo);
+  }
+}
 
 void merge_sort(int a[], int size){
-  /* TODO */
+  merge_sort_top(a, 0, size-1);
 }
